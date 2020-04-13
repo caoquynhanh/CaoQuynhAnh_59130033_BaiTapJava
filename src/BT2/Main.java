655ac98a6617084556d2630b811d6c9540ac6f39
Asus <Asus@DESKTOP-T4GSA55>
2020-04-13 21:01:07 +0700

    fix1

--- src/BT2/Main.java
+++ src/BT2/Main.java
@@ -9,49 +9,50 @@ package BT2;
  *
  * @author Asus
  */
-public class Main {
-     public static void main(String[] args) {
+public class Main 
+{
+    public static void main(String[] args) {
     // create quan ly nhan vien object
     IQuanLy qlNhanVien = new QuanLyNhanVien();
 
     // create nhan vien
     NhanVien nv1 = new NhanVien();
-    nv1.setTen("Nguyen Van");
-    nv1.setTuoi(20);
-    nv1.setDiaChi("Nha Trang - Khanh Hoa");
-    nv1.setTienLuong(10000000);
-    nv1.setTongGioLam(100);
+    nv1.setTen("Hoang Tuan Kien");
+    nv1.setTuoi(31);
+    nv1.setDiaChi("Ha Noi");
+    nv1.setTienLuong(6000000);
+    nv1.setTongGioLam(150);
 
     // create nhan vien
     NhanVien nv2 = new NhanVien();
-    nv2.setTen("Nguyen Van B");
-    nv2.setTuoi(21);
-    nv2.setDiaChi("Nha Trang - Khanh Hoa");
-    nv2.setTienLuong(20000000);
-    nv2.setTongGioLam(200);
+    nv2.setTen("Nguyen Thai Son");
+    nv2.setTuoi(23);
+    nv2.setDiaChi("Quang Ninh");
+    nv2.setTienLuong(7000000);
+    nv2.setTongGioLam(300);
 
     // create nhan vien
     NhanVien nv3 = new NhanVien();
-    nv3.setTen("Nguyen Van C");
+    nv3.setTen("Pham Thu Trang");
     nv3.setTuoi(22);
-    nv3.setDiaChi("Nha Trang - Khanh Hoa");
-    nv3.setTienLuong(18000000);
-    nv3.setTongGioLam(200);
+    nv3.setDiaChi("Ha Noi");
+    nv3.setTienLuong(5000000);
+    nv3.setTongGioLam(100);
 
     // create nhan vien
     NhanVien nv4 = new NhanVien();
-    nv4.setTen("Nguyen Van D");
+    nv4.setTen("Nguyen Nam Thuong");
     nv4.setTuoi(23);
-    nv4.setDiaChi("Tuy Hoa - Phu Yen");
-    nv4.setTienLuong(25000000);
-    nv4.setTongGioLam(300);
+    nv4.setDiaChi("Thanh Hoa");
+    nv4.setTienLuong(6500000);
+    nv4.setTongGioLam(200);
 
     // create nhan vien
     NhanVien nv5 = new NhanVien();
-    nv5.setTen("Nguyen Van E");
-    nv5.setTuoi(24);
-    nv5.setDiaChi("Nha Trang - Khanh Hoa");
-    nv5.setTienLuong(150000000);
-    nv5.setTongGioLam(100);
+    nv5.setTen("Hoang Thanh Loan");
+    nv5.setTuoi(30);
+    nv5.setDiaChi("Nam Dinh");
+    nv5.setTienLuong(6500000);
+    nv5.setTongGioLam(200);
   }
 }
