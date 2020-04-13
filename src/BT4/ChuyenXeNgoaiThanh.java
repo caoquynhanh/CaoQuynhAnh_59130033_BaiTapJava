25f1b662b9c535b24a228185eb2edaa2e61c7ea9
Asus <Asus@DESKTOP-T4GSA55>
2020-04-13 22:00:52 +0700

    fix1

---
+++ src/BT4/ChuyenXeNgoaiThanh.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BT4;
+
+/**
+ *
+ * @author Asus
+ */
+public class ChuyenXeNgoaiThanh extends ChuyenXe 
+{
+    // attributes
+    public String noiDen;
+    public int soNgay;
+  
+    // constructor
+    public ChuyenXeNgoaiThanh(int maChuyen, String taiXe, String soXe, double doanhThu, String noiDen, int soNgay) 
+    {
+      super(maChuyen, taiXe, soXe, doanhThu);
+      this.noiDen = noiDen;
+      this.soNgay = soNgay;
+    }
+
+    // methods
+    // in thong tin chuyen xe
+    @Override
+    public void inThongTinChuyenXe() 
+    {
+      super.inThongTinChuyenXe();
+      System.out.println("noi den: " + this.noiDen);
+      System.out.println("so ngay: " + this.soNgay);
+    }
+}
