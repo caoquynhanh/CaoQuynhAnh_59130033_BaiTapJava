e1780110501711f5780b40ea8e175aa665f1c335
Asus <Asus@DESKTOP-T4GSA55>
2020-04-13 22:01:10 +0700

    fix1

---
+++ src/BT4/ChuyenXeNoiThanh.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BT4;
+
+/**
+ *
+ * @author Asus
+ */
+public class ChuyenXeNoiThanh extends ChuyenXe
+{
+    // attributes
+    public String soTuyen;
+    public double soKm;
+
+    // constructor
+    public ChuyenXeNoiThanh(int maChuyen, String taiXe, String soXe, double doanhThu, String soTuyen, double soKm) 
+    {
+      super(maChuyen, taiXe, soXe, doanhThu);
+      this.soTuyen = soTuyen;
+      this.soKm = soKm;
+    }
+
+    // methods
+    // in thong tin
+    @Override
+    public void inThongTinChuyenXe() 
+    {
+      super.inThongTinChuyenXe();
+      System.out.println("tuyen: " + this.soTuyen);
+      System.out.println("so km di duoc: " + this.soKm);
+    }
+}
