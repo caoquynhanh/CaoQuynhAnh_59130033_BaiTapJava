fbfc340c862bca5ee1fcc88b8a84c163d890bedc
Asus <Asus@DESKTOP-T4GSA55>
2020-04-13 21:46:09 +0700

    fix1

---
+++ src/BT3/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BT3;
+
+/**
+ *
+ * @author Asus
+ */
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        // create sinh vien IT
+        SinhVienIT it = new SinhVienIT("Tran Ba Duy", "CNTT", 6.5, 7, );
+        // create sinh vien Biz
+        SinhVienBiz biz = new SinhVienBiz("Nguyen Tung Son", "Biz", 7.5, 10);
+        // create sinh vien poly tu sinh vien IT
+        SinhVienPoly poly = new SinhVienIT("Bui Bao Chau", "CNTT", 4, 9, 7.5);
+
+    }
+}
