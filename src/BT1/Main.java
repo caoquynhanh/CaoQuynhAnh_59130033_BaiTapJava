29df5de7814f55edd9dd5635cad7123bf4bf7fae
Asus <Asus@DESKTOP-T4GSA55>
2020-04-13 20:44:24 +0700

    fix1

--- src/BT1/Main.java
+++ src/BT1/Main.java
@@ -9,26 +9,28 @@ package BT1;
  *
  * @author Asus
  */
-public class Main {
-     public static void main(String[] args) {
+public class Main 
+{
+    public static void main(String[] args) 
+    {
         // create nhan vien
         NhanVien nv1 = new NhanVien();
-        nv1.setTen("Le Thi Thu Thuy");
-        nv1.setTuoi(33);
-        nv1.setDiaChi("Phu Yen");
+        nv1.setTen("Hoang Tuan Kien");
+        nv1.setTuoi(31);
+        nv1.setDiaChi("Ha Noi");
         nv1.setTienLuong(6000000);
-        nv1.setTongGioLam(200);
+        nv1.setTongSoGioLam(150);
         
         // create nhan vien
         NhanVien nv2 = new NhanVien();
-        nv2.setTen("Le Thi Dep");
-        nv2.setTuoi(44);
-        nv2.setDiaChi("Phu Yen");
+        nv2.setTen("Nguyen Thai Son");
+        nv2.setTuoi(23);
+        nv2.setDiaChi("Quang Ninh");
         nv2.setTienLuong(7000000);
-        nv2.setTongGioLam(300);
+        nv2.setTongSoGioLam(300);
 
         // in thong tin nhan vien
-        System.out.print(nv1.getThongTin());
-        System.out.print(nv2.getThongTin());
-  } 
+        System.out.println(nv1.getThongTin());
+        System.out.println(nv2.getThongTin());
+    } 
 }
