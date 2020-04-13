f520d00373ab693f284148ccb9cb50dc8bcaa254
Asus <Asus@DESKTOP-T4GSA55>
2020-04-13 21:49:01 +0700

    fix1

---
+++ src/BT3/SinhVienIT.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BT3;
+
+/**
+ *
+ * @author Asus
+ */
+public class SinhVienIT extends SinhVienPoly 
+{
+    // attributes
+    public double diemJava;
+    public double diemCss;
+    public double diemHtml;
+
+    // constructor
+    public SinhVienIT(String hoTen, String nganhHoc, double diemJava, double diemCss, double diemHtml) 
+    {
+        super(hoTen, nganhHoc);
+        this.diemJava = diemJava;
+        this.diemCss = diemCss;
+        this.diemHtml = diemHtml;
+    }
+
+
+}
