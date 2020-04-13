012bf9b399adb89a99c63c5bbfc15086794722d9
Asus <Asus@DESKTOP-T4GSA55>
2020-04-13 22:00:36 +0700

    fix1

---
+++ src/BT4/ChuyenXe.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BT4;
+
+/**
+ *
+ * @author Asus
+ */
+public abstract class ChuyenXe 
+{
+    // attributes
+    public int maChuyen;
+    public String taiXe;
+    public String soXe;
+    public double doanhThu;
+
+    // contructor
+    public ChuyenXe(int maChuyen, String taiXe, String soXe, double doanhThu) 
+    {
+      this.maChuyen = maChuyen;
+      this.taiXe = taiXe;
+      this.soXe = soXe;
+      this.doanhThu = doanhThu;
+    }
+
+    // methods
+    // in thong tin xe
+    public void inThongTinChuyenXe() 
+    {
+      System.out.println("ma chuyen xe: " + this.maChuyen);
+      System.out.println("ten tai xe: " + this.taiXe);
+      System.out.println("so xe: " + this.soXe);
+      System.out.printf("doanh thu: %.00f\n", this.doanhThu);
+    };
+}
