48e6214606d2d694127511ee3ee11687efe08b46
Asus <Asus@DESKTOP-T4GSA55>
2020-04-13 21:59:33 +0700

    fix1

--- src/BT3/Main.java
+++ src/BT3/Main.java
@@ -20,5 +20,17 @@ public class Main
         // create sinh vien poly tu sinh vien IT
         SinhVienPoly poly = new SinhVienIT("Bui Bao Chau", "CNTT", 4, 9, 7.5);
 
+        // xuat thong tin sinh sinh
+        it.xuat();
+        System.out.println("diem: " + it.getDiem());
+        System.out.println("hoc luc: " + it.getHocLuc());
+
+        biz.xuat();
+        System.out.println("diem: " + biz.getDiem());
+        System.out.println("hoc luc: " + biz.getHocLuc());
+
+        poly.xuat();
+        System.out.println("diem: " + poly.getDiem());
+        System.out.println("hoc luc: " + poly.getHocLuc());
     }
 }
