6099b13d82c4866b99234f16a56bfefb7a610309
Asus <Asus@DESKTOP-T4GSA55>
2020-04-13 21:48:40 +0700

    fix1

---
+++ src/BT3/SinhVienBiz.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BT3;
+
+/**
+ *
+ * @author Asus
+ */
+public class SinhVienBiz extends SinhVienPoly 
+{
+    // attributes
+    public double diemMarketing;
+    public double diemSales;
+
+    // constructor
+    public SinhVienBiz(String hoTen, String nganhHoc, double diemMarketing, double diemSales) 
+    {
+        super(hoTen, nganhHoc);
+        this.diemMarketing = diemMarketing;
+        this.diemSales = diemSales;
+    }
+
+
+}
