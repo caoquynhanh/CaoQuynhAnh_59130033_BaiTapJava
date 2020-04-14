e6dc35a0f34f3861667403446f302e8d7cc9cd89
Asus <Asus@DESKTOP-T4GSA55>
2020-04-14 16:09:26 +0700

    fix3

---
+++ src/BTSoDo/IQLDS.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BTSoDo;
+
+/**
+ *
+ * @author Asus
+ */
+public interface IQLDS 
+{
+  public abstract int them(BT1.CaNhan p);
+  public abstract int xoa(String ten);
+  public abstract void inDS();   
+}
